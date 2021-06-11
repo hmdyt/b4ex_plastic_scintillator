c1760c084f8fdafe3b86f7261d7f0f9fabad863e
hmdyt <konnyaku108@gmail.com>
2021-06-11 15:31:03 +0900

    kore mite atode yaru

--- src/week1/fitLED3005_CH26.C
+++ src/week1/fitLED3005_CH26.C
@@ -5,6 +5,17 @@ void fitLED3005_CH26(){
     const Double_t XMIN = 1400;
     const Double_t XMAX = 3300;
 
+    // init fitting propaties
+    // initial const, mean sigma are FITHEIGHT ,FITMIDMIN~MAX, FITWIDTH
+    const Double_t FITHEIGHT = 20;
+    const Double_t FITMIDMIN = 1550;
+    const Double_t FITMIDMAX = 2900;
+    const Double_t FITWIDTH = 50;
+    const Double_t FITRANGEMAX = FITMIDMIN - FITWIDTH/2;
+    const Double_t FITRANGEMIN = FITMIDMAX + FITWIDTH/2;
+    const Int_t GAUSNUM = (Int_t) (FITMIDMAX - FITMIDMIN) / FITWIDTH;
+    const TString MULTIGAUS = getMultiGaus(GAUSNUM);
+
     // fetch hist
     TH1D* hist = getHistMPPC(FILENAME, CHANNEL);
 
@@ -13,6 +24,22 @@ void fitLED3005_CH26(){
 
     // arrange hist title
     hist->SetTitle(FILENAME + " " + CHANNEL);
+
+    // init TF1
+    TF1* f = new TF1("f", MULTIGAUS, FITRANGEMIN, FITRANGEMAX);
+
+    // set parnames ane initial params
+    for (Int_t i=0; i<GAUSNUM; i++) {
+        f->SetParName(3*i + 0, (TString)to_string(i) + (TString)"th Const");
+        f->SetParName(3*i + 1, (TString)to_string(i) + (TString)"th Mean"); 
+        f->SetParName(3*i + 2, (TString)to_string(i) + (TString)"th Sigma"); 
+        f->SetParameter(3*i + 0, FITHEIGHT);
+        f->SetParameter(3*i + 1, FITMIDMIN + FITWIDTH * i);
+        f->SetParameter(3*i + 2, FITWIDTH);
+     }
+    
+    // execute fitting
+    hist->Fit(f, "R");
 
     // draw and save
     auto c = new TCanvas();
