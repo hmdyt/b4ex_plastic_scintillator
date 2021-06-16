e3cb7cb20bc8af135894c9cbcdf82c954b9d8422
hmdyt <konnyaku108@gmail.com>
2021-06-16 16:09:40 +0900

    add getpedestalmean

--- rootlogon.C
+++ rootlogon.C
@@ -28,7 +28,7 @@ void rootlogon(){
     loadMacro("src/week1/fitLED3020_CH29.C");
     loadMacro("src/week1/fitLED3021_CH26.C");
     loadMacro("src/week1/fitLED3021_CH29.C");
-    loadMacro("src/week1/fitLEDXXXX_CHXX_Pedestal.C");
+    loadMacro("src/week1/getPedestalMean.C");
     loadMacro("src/week1/determineMeanWidth.C");
     loadMacro("src/week1/drawVoltWidthGraph.C");
 }
